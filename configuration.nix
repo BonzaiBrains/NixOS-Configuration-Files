0d04f4ae9c4579fc38b0eee764bc6b1377849e2d
BonzaiBrains <ahoemann@proton.me>
2023-07-28 18:09:14 +0200

    Font change

--- configuration.nix
+++ configuration.nix
@@ -152,7 +152,7 @@
       enable = true;
       extraConfig = ''
         set-option -g mouse on
-        set -g status-right '#[fg=black,bg=color15] #{cpu_percentage}  %H:%M '
+        set -g status-right '#[fg=black,bg=color15] #{cpu_percentage} %H:%M '
         run-shell ${pkgs.tmuxPlugins.cpu}/share/tmux-plugins/cpu/cpu.tmux
       '';
     };
