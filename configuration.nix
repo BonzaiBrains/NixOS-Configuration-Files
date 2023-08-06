d7c47d83d7e4fbc35b1985d6b5ed91e3003b9843
BonzaiBrains <ahoemann@proton.me>
2023-08-06 13:59:01 +0200

    System specific config move to hardwar-configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -16,7 +16,6 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "workstation"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   
   security.polkit.enable = true;
@@ -182,14 +181,6 @@
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.05"; # Did you read the comment?
   
   nix = {
     settings.experimental-features = [ "nix-command" "flakes" ];
