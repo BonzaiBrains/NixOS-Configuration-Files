b2c9160e501f1e1b57042414e5d8cc36511cf3e5
BonzaiBrains <ahoemann@proton.me>
2023-08-21 18:07:36 +0200

    Update

--- configuration.nix
+++ configuration.nix
@@ -151,6 +151,7 @@
     tmux = {
       enable = true;
       extraConfig = ''
+        set -g default-shell fish
         set-option -g mouse on
         set -g status-right '#[fg=black,bg=color15] #{cpu_percentage} %H:%M '
         run-shell ${pkgs.tmuxPlugins.cpu}/share/tmux-plugins/cpu/cpu.tmux
